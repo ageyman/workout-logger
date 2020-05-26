f4eca3a9e8991b2ecdfeb0066606cf09d4620f0f
Aleksandar Geyman <aleksandar.geyman@upnetix.com>
2020-05-26 22:41:15 +0300

    initial setup for coordinator

---
+++ WorkoutLogger/Coordinator/Coordinatable.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinatable.swift
+//  WorkoutLogger
+//
+//  Created by Aleksandar Geyman on 26.05.20.
+//  Copyright © 2020 Aleksandar Geyman. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinatable: class {
+    
+    func start()
+}
