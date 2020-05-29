2a198b936b88f84184afbeee56456e61dba6a214
Aleksandar Geyman <aleksandar.geyman@upnetix.com>
2020-05-29 22:38:03 +0300

    removed header view

--- WorkoutLogger/Modules/AddWorkoutModule/AddWorkoutVC.swift
+++ WorkoutLogger/Modules/AddWorkoutModule/AddWorkoutVC.swift
@@ -23,9 +23,6 @@ class AddWorkoutVC: BaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(with: viewModel)
-        let headerView = ExercisesHeaderView(frame: CGRect(x: 0, y: 0, width: 0, height: 60))
-        headerView.configure(with: ExercisesHeaderViewModel(workoutName: "Workout"))
-        tableView.tableHeaderView = headerView
     }
 }
 
